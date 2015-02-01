d1a5340b99618e761baebe08a2fe2bef1101a20d
Dmitriy Pilipenko <dmitriy.pilipenko@gmail.com>
2015-02-01 14:51:59 -0500

    download the files

--- SimpleAzureBlobFetcher/AzureClient.cs
+++ SimpleAzureBlobFetcher/AzureClient.cs
@@ -7,36 +7,45 @@ using System.Threading.Tasks;
 
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
+using System.IO;
 
 namespace SimpleAzureBlobFetcher {
     class AzureClient {
 
-        public void Run(string acctName, string acctKey) {
+        public void Run(string acctName, string acctKey, DirectoryInfo dir) {
             string connectionString = "DefaultEndpointsProtocol=https;AccountName=" + acctName + ";AccountKey=" + acctKey;
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
 
-            Console.WriteLine("Azure Client Connected.");
-
+            Console.WriteLine("Azure Client Connected");
             CloudBlobClient client = storageAccount.CreateCloudBlobClient();
-            ProcessContainers(client.ListContainers());
+            
+            Console.WriteLine("Begin downloading of files...");
+            ProcessContainers(client.ListContainers(), dir);
+            Console.WriteLine("...Downloaded all files");
         }
 
-        private void ProcessContainers(IEnumerable<CloudBlobContainer> enumerable) {
+        private void ProcessContainers(IEnumerable<CloudBlobContainer> enumerable, DirectoryInfo dir) {
             int counter = 0;
             foreach (CloudBlobContainer container in enumerable) {
                 counter++;
-                Console.WriteLine("Container {0}: {1}", counter, container.Name);
-                ProcessBlobs(container.ListBlobs());
+
+                DirectoryInfo containerDir = dir.CreateSubdirectory(container.Name);
+
+                ProcessBlobs(container.ListBlobs(), containerDir);
             }
         }
 
-        private void ProcessBlobs(IEnumerable<IListBlobItem> enumerable) {
+        private void ProcessBlobs(IEnumerable<IListBlobItem> enumerable, DirectoryInfo dir) {
             int counter = 0;
             foreach (IListBlobItem item in enumerable) {
                 if (item.GetType() == typeof(CloudBlockBlob)) {
                     counter++;
                     CloudBlockBlob blob = (CloudBlockBlob)item;
-                    Console.WriteLine("\tBlob {0}: {1}", counter, blob.Name);
+
+                    using (var fileStream = System.IO.File.OpenWrite(dir.ToString() + @"\" + blob.Name)) {
+                        blob.DownloadToStream(fileStream);
+                    }
+
                 }
             }
         }
--- SimpleAzureBlobFetcher/Program.cs
+++ SimpleAzureBlobFetcher/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace SimpleAzureBlobFetcher {
     class Program {
@@ -10,7 +13,10 @@ namespace SimpleAzureBlobFetcher {
         static int EXPECTED_ARG_LENGTH = 2;
 
         static void Main(string[] args) {
-            Console.WriteLine("Start.");
+            Console.WriteLine("Start");
+
+            Stopwatch stopwatch = new Stopwatch();
+
             if (args.Length < EXPECTED_ARG_LENGTH) {
                 HandleInputTooLittle();
             } else 
@@ -20,9 +26,11 @@ namespace SimpleAzureBlobFetcher {
             } else
 
             if (args.Length == EXPECTED_ARG_LENGTH) {
+                stopwatch.Start();
                 HandleInput(args);
+                stopwatch.Stop();
             }
-            Console.WriteLine("Done.");
+            Console.WriteLine(String.Format("Done. Took {0} seconds", stopwatch.ElapsedMilliseconds / 1000));
             Console.ReadLine();
         }
 
@@ -30,8 +38,27 @@ namespace SimpleAzureBlobFetcher {
             string ACCT_NAME = args[0];
             string ACCT_KEY = args[1];
 
+            DirectoryInfo outputDir = GetOrCreateOutputFolder();
 
-            new AzureClient().Run(ACCT_NAME, ACCT_KEY);
+            new AzureClient().Run(ACCT_NAME, ACCT_KEY, outputDir);
+        }
+
+        private static DirectoryInfo GetOrCreateOutputFolder() {
+            DirectoryInfo dir = new DirectoryInfo("Output");
+            if (dir.Exists) {
+                Console.WriteLine("Folder ./Output/ already exists, deleting contents");
+                foreach (DirectoryInfo info in dir.GetDirectories()) {
+                    info.Delete(true);
+                }
+                foreach (FileInfo info in dir.GetFiles()) {
+                    info.Delete();
+                }
+                Console.WriteLine("Deleted.");
+            } else {
+                dir.Create();
+                Console.WriteLine("Created folder ./Output/");
+            }
+            return dir;
         }
 
         private static void HandleInputTooLittle() {
