8bc9c75a3479a9b63daf9f18a7aa20c751ba6634
Dmitriy Pilipenko <dmitriy.pilipenko@gmail.com>
2015-02-01 12:23:28 -0500

    add program

---
+++ SimpleAzureBlobFetcher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAzureBlobFetcher {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Start.");
+            if (args.Length == 2) {
+                var arg1 = args[0];
+                var arg2 = args[1];
+                Console.WriteLine(String.Format("Argument {0}: {1}", 0, arg1));
+                Console.WriteLine(String.Format("Argument {0}: {1}", 1, arg2));
+            }
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+    }
+}
