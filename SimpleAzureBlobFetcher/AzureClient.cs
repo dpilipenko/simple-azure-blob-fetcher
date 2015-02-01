73a82a5743b6bcfa9e3cb4973a11c902098ad9a3
Dmitriy Pilipenko <dmitriy.pilipenko@gmail.com>
2015-02-01 13:26:40 -0500

    Connects with Azure

---
+++ SimpleAzureBlobFetcher/AzureClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace SimpleAzureBlobFetcher {
+    class AzureClient {
+
+        public void Run(string acctName, string acctKey) {
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName="+acctName+";AccountKey="+acctKey;
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+
+            Console.WriteLine("Azure Client Connected.");
+            Console.WriteLine(storageAccount.FileStorageUri);
+        }
+
+    }
+}
--- SimpleAzureBlobFetcher/Program.cs
+++ SimpleAzureBlobFetcher/Program.cs
@@ -6,16 +6,40 @@ using System.Threading.Tasks;
 
 namespace SimpleAzureBlobFetcher {
     class Program {
+
+        static int EXPECTED_ARG_LENGTH = 2;
+
         static void Main(string[] args) {
             Console.WriteLine("Start.");
-            if (args.Length == 2) {
-                var arg1 = args[0];
-                var arg2 = args[1];
-                Console.WriteLine(String.Format("Argument {0}: {1}", 0, arg1));
-                Console.WriteLine(String.Format("Argument {0}: {1}", 1, arg2));
+            if (args.Length < EXPECTED_ARG_LENGTH) {
+                HandleInputTooLittle();
+            } else 
+
+            if (args.Length > EXPECTED_ARG_LENGTH) {
+                HandleInputTooLarge();
+            } else
+
+            if (args.Length == EXPECTED_ARG_LENGTH) {
+                HandleInput(args);
             }
             Console.WriteLine("Done.");
             Console.ReadLine();
+        }
+
+        private static void HandleInput(string[] args) {
+            string ACCT_NAME = args[0];
+            string ACCT_KEY = args[1];
+
+
+            new AzureClient().Run(ACCT_NAME, ACCT_KEY);
+        }
+
+        private static void HandleInputTooLittle() {
+            Console.WriteLine(String.Format("Please Include Only {0} Arguments: API_KEY API_SECRET"), EXPECTED_ARG_LENGTH);
+        }
+
+        private static void HandleInputTooLarge() {
+            Console.WriteLine(String.Format("Please Include Only {0} Arguments: API_KEY API_SECRET"), EXPECTED_ARG_LENGTH);
         }
     }
 }
